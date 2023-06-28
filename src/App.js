0932d2f047bdf1e8123bd8645d549e4bae8c884f
unknown <ravikumargupta391999@gmail.com>
2023-06-28 11:17:22 +0530

    Providing Store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { Provider } from "react-redux";
 import TodoForm from "./components/ToDoForm/ToDoForm";
 import TodoList from "./components/ToDoList/ToDoList";
 
+import store from "./redux/store"
 import './App.css';
 
 function App() {
@@ -20,9 +22,13 @@ function App() {
   return (
     <div>
       <h1>To Do App</h1>
+      
 
-      <TodoForm onCreateTodo={createTodo} />
-      <TodoList todos={todos} onToggle={toggleTodo} />
+    <Provider store={store}>
+       <TodoForm onCreateTodo={createTodo} />
+       <TodoList todos={todos} onToggle={toggleTodo} />
+    </Provider>
+      
     </div>
   );
 }
